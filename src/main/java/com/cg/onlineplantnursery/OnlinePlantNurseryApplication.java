26974e507dc866f2a3314bebb408f421f0586531
anirudh49 <anirudhbhardwaj4444@gmail.com>
2021-03-26 15:13:23 +0530

    seed updated

--- src/main/java/com/cg/onlineplantnursery/OnlinePlantNurseryApplication.java
+++ src/main/java/com/cg/onlineplantnursery/OnlinePlantNurseryApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.cg.onlineplantnursery.seed.ui.SeedUI;
+
 @SpringBootApplication
 public class OnlinePlantNurseryApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(OnlinePlantNurseryApplication.class, args);
-
+		SeedUI ui = context.getBean(SeedUI.class);
+		ui.start();
 	}
 
 }
