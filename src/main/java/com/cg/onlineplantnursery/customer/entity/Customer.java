a74a84a9dc6a203ebc0705b79e4d4a14e49d24fd
Kumar Rohit <rohit.kumar01091997@gmail.com>
2021-03-28 14:33:25 +0530

    reverted customer changes

--- src/main/java/com/cg/onlineplantnursery/customer/entity/Customer.java
+++ src/main/java/com/cg/onlineplantnursery/customer/entity/Customer.java
@@ -2,7 +2,6 @@ package com.cg.onlineplantnursery.customer.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Customer {
@@ -14,13 +13,19 @@ public class Customer {
 	private String customerEmail;
 	private String username;
 	private String password;
-	
-	@OneToOne
-	private Address address;
+	//private Address address;
 	
 	public Customer() {
 		
 	}
+	/*public Customer( String customerName,String customerEmail, String username, String password) { //Address address 
+		//this.customerId=customerId;
+		this.customerName=customerName;
+		this.customerEmail=customerEmail;
+		this.username=username;
+		this.password=password;
+		//this.address=address;
+	}*/
 	
 	public Integer getCustomerId() {
 		return customerId;
@@ -67,6 +72,7 @@ public class Customer {
 		this.password=password;
 	}
 	
+	/*
 	public Address getAddress() {
 		return address;
 		
@@ -75,6 +81,7 @@ public class Customer {
 	public void setAddress(Address address) {
 		this.address=address;
 	}
+	*/
 	
 	@Override
 	public int hashCode() {
