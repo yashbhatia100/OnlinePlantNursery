59dc9446c775ced551b6943ea0dcd9cec2fd03ef
chandan <chandansharma7750@gmail.com>
2021-03-28 19:44:45 +0530

    order

--- src/main/java/com/cg/onlineplantnursery/OnlinePlantNurseryApplication.java
+++ src/main/java/com/cg/onlineplantnursery/OnlinePlantNurseryApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.cg.onlineplantnursery.customer.ui.CustomerUI;
+import com.cg.onlineplantnursery.order.ui.OrderUi;
 import com.cg.onlineplantnursery.plant.ui.PlantUI;
 import com.cg.onlineplantnursery.planter.ui.PlanterConsoleUi;
 import com.cg.onlineplantnursery.seed.ui.SeedUI;
@@ -13,7 +14,7 @@ import com.cg.onlineplantnursery.seed.ui.SeedUI;
 public class OnlinePlantNurseryApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(OnlinePlantNurseryApplication.class, args);
-
+/*
 		PlantUI app1 = context.getBean(PlantUI.class);
 		app1.start();
 		
@@ -26,6 +27,10 @@ public class OnlinePlantNurseryApplication {
 		SeedUI app4 = context.getBean(SeedUI.class);
 		app4.start();
 		
+		
+		OrderUi app5=context.getBean(OrderUi.class);
+		app5.start();
+		*/
 		
 	}
 
